6dcae3d555c5de35eaf549abccc709e583360318
Sam Narisi <snarisi@gmail.com>
2016-01-28 11:00:15 -0500

    Trying to test the new presentation route

--- browser/js/presentations/presentations.js
+++ browser/js/presentations/presentations.js
@@ -1,7 +1,7 @@
 app.config(function ($stateProvider) {
 
     $stateProvider.state('editPres', {
-        url: 'presentations/:id/edit',
+        url: '/presentations/:id/edit',
         templateUrl: 'js/presentations/edit.html',
         controller: function ($scope, $stateParams) {
             console.log('from state params: ', $stateParams.id);
