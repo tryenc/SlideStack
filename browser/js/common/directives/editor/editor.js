7325d36047513da86cf73a054bde2b053aae7fe6
Sam Narisi <snarisi@gmail.com>
2016-02-01 11:21:50 -0500

    Trying to get editor to now crash while live editing

--- browser/js/common/directives/editor/editor.js
+++ browser/js/common/directives/editor/editor.js
@@ -2,6 +2,7 @@ app.directive('editor', function () {
     return {
         restrict: 'E',
         scope: {},
+        transclude: true,
         templateUrl: 'js/common/directives/editor/editor.html',
         link: function (scope, element, attrs) {
         }
