6819180b0a12836548f2133e37d32e1c6618e11e
Sam Narisi <snarisi@gmail.com>
2016-02-03 14:42:40 -0500

    Added student presentation view

--- browser/js/common/directives/slideshow/slideshow.js
+++ browser/js/common/directives/slideshow/slideshow.js
@@ -8,7 +8,7 @@ app.directive('ssSlideshow', function () {
         templateUrl: 'js/common/directives/slideshow/slideshow.html',
         controller: function ($scope) {
 
-            var slides = [];
+            const slides = [];
             $scope.currentSlide = 0;
 
             this.addSlide = function (slide) {
@@ -27,13 +27,13 @@ app.directive('ssSlideshow', function () {
             };
 
             document.body.addEventListener('keyup', function (e) {
+                if ($scope.display.mode === 'student' && $scope.syncedWithTeacher) return;
+
                 if (e.which === 39) {
-                    e.preventDefault();
                     $scope.next();
                     $scope.$digest();
                 }
                 if (e.which === 37) {
-                    e.preventDefault();
                     $scope.prev();
                     $scope.$digest();
                 }
@@ -44,7 +44,33 @@ app.directive('ssSlideshow', function () {
 
                 slides[oldIdx].selected = false;
                 slides[newIdx].selected = true;
+
+                // if ($scope.display.mode === 'teacher') {
+                //     socket.broadcast.emit('change slide', newIdx)
+                // }
             });
+
+            // controls for student view
+            if ($scope.display.mode !== 'student') return;
+
+            let currentTeacherSlide = 0;
+            $scope.syncedWithTeacher = true;
+
+            $scope.toggleSync = function () {
+                if (!$scope.syncedWithTeacher) {
+                    $scope.currentSlide = currentTeacherSlide;
+                }
+                $scope.syncedWithTeacher = !$scope.syncedWithTeacher;
+            }
+
+            // handle socket events
+            // socket.on('change slide', function (slideNumber) {
+            //     currentTeacherSlide = slideNumber;
+            //     if ($scope.syncedWithTeacher) {
+            //         $scope.currentSlide = currentTeacherSlide;
+            //     }
+            // })
+
 
         }
     }
