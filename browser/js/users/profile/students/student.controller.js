044e838e4db0b4b31392136e3e87516a3e5b0e0d
Jon Horn <jhorn85@gmail.com>
2016-02-11 22:12:31 -0500

    more styling for user profile

--- browser/js/users/profile/students/student.controller.js
+++ browser/js/users/profile/students/student.controller.js
@@ -5,23 +5,4 @@
 app.controller('StudentsTabCtrl', ($scope, UserFactory, ClassFactory, classes) => {
   $scope.classes = classes;
 
-})
-//
-//// Filter for teacher/student dropdown showing only available classes
-//.filter('NotEnrolled', () => {
-//    return (teacherArray, student) => {
-//
-//        var teacherArrayCopy = teacherArray.slice();
-//        for (var i = 0; i < teacherArrayCopy.length; i++) {
-//            var currentTeacher = teacherArrayCopy[i];
-//            for (var j = 0; j < student.classes.length; j++) {
-//                var studentCurrent = student.classes[j];
-//                if (currentTeacher._id === studentCurrent._id) {
-//                    teacherArrayCopy.splice(i, 1);
-//                    i--;
-//                }
-//            }
-//        }
-//        return teacherArrayCopy;
-//    }
-//});
+});
